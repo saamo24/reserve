50d04e6bbdc9dd0b9a1bc14d3915a0bf0f880ea1
samvel <samvel.abrahamyan@imeasystems.com>
2026-02-24 17:25:54 +0400

    minor fix in timzone

--- app/api/public/__init__.py
+++ app/api/public/__init__.py
@@ -4,7 +4,9 @@ from fastapi import APIRouter
 
 from app.api.public.branches import router as branches_router
 from app.api.public.reservation import router as reservations_router
+from app.api.public.timezones import router as timezones_router
 
 public_router = APIRouter()
 public_router.include_router(branches_router)
 public_router.include_router(reservations_router)
+public_router.include_router(timezones_router)
